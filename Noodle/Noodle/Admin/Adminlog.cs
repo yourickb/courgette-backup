ed596019aad5d0e2e4505803584ea4811b7424a5
yourickb <1032606@hr.nl>
2022-04-19 21:36:11 +0200

    Update Adminscherm

--- Noodle/Noodle/Admin/Adminlog.cs
+++ Noodle/Noodle/Admin/Adminlog.cs
@@ -6,13 +6,88 @@ using System.Text.Json;
 
 namespace Noodle
 {
-
     class Admin : Step
     {
+        private readonly Step _option1_1;
+        private readonly Step _option1_2;
+        private readonly Step _option1_3;
+        private readonly Step _option1_4;
+
         public Admin()
         {
+            _option1_1 = new ResOverzicht();
+            _option1_2 = new ResOverzicht();
+            _option1_3 = new ResOverzicht();
+            _option1_4 = new ResOverzicht();
 
+            _option1_1.SetPrevious(this);
+            _option1_2.SetPrevious(this);
+            _option1_3.SetPrevious(this);
+            _option1_4.SetPrevious(this);
+        }
+        public override void Show()
+        {
+            Console.Clear();
+            Log("[Step 2]");
+            var greetingsJson = new WelcomePage();
+            string taalSetting = greetingsJson.Getlanguage();
+            ConsoleKeyInfo input;
+
+            do
+            {
+                Console.Clear();
+
+                if (taalSetting == "nl")
+                {
+                    Display("Kies alstublieft uit één van de volgende opties: (dit gaat allemaal nog alleen naar de AdminOverzichtReservering json file)");
+                    Display("");
+                    Display("[1] Overzicht van de reserveringen");
+                    Display("[2] Overzicht van de tafels");
+                    Display("[3] Bestellingen per tafel");
+                    Display("[4] Aanpassen van het menu");
+                    Display("");
+                }
+
+                else
+                {
+                    Display("Please choose one of the following options:");
+                    Display("");
+                    Display("[1] Overview of the reservations");
+                    Display("[2] Overview of the tables");
+                    Display("[3] Orders per table");
+                    Display("[4] Change the menu");
+                    Display("");
+                }
+
+                input = Console.ReadKey();
+            }
+            while (input.Key != ConsoleKey.D1 && input.Key != ConsoleKey.D2 && input.Key != ConsoleKey.D3 && input.Key != ConsoleKey.D4 && input.Key != ConsoleKey.Escape);
+
+            if (input.Key == ConsoleKey.D1)
+            {
+                _option1_1.Show();
+            }
+
+            if (input.Key == ConsoleKey.D2)
+            {
+                _option1_2.Show();
+            }
+
+            if (input.Key == ConsoleKey.D3)
+            {
+                _option1_3.Show();
+            }
+            
+            if (input.Key == ConsoleKey.D4)
+            {
+                _option1_4.Show();
+            }
+            
+            if (input.Key == ConsoleKey.Escape)
+            {
+                var MainMenu = new MainMenu();
+                MainMenu.Show();
+            }
         }
     }
-
 }
---
+++ Noodle/Noodle/Admin/ResOverzicht.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.Json;
+
+namespace Noodle
+{
+
+    class ResOverzicht : Step
+    {
+        public class AdminOverzichtReservering
+        {
+            public string Overzicht { get; set; }
+        }
+
+        public AdminOverzichtReservering Deserialize()
+        {
+        string json = File.ReadAllText("AdminOverzichtReservering.json");
+        var AdminOverzichtReserveringJson = JsonSerializer.Deserialize<AdminOverzichtReservering>(json);
+        return AdminOverzichtReserveringJson;
+        }
+
+        public override void Show()
+        {
+            Log("");
+            var greetingsJson = new WelcomePage();
+            string taalSetting = greetingsJson.Getlanguage();
+            ConsoleKeyInfo input;
+
+            do
+            {
+                {
+                    Console.Clear();
+                    var AdminOverzichtReserveringJson = Deserialize();
+                    if (taalSetting == "nl")
+                    {
+                        Console.WriteLine("Hier is het overzicht van de reserveringen: ");
+                        Console.WriteLine(AdminOverzichtReserveringJson.Overzicht);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Here is the overview of the reservations: ");
+                        Console.WriteLine(AdminOverzichtReserveringJson.Overzicht);
+                    }
+
+                    input = Console.ReadKey();
+
+                }
+            }
+            while (input.Key != ConsoleKey.Escape);
+            
+            if (input.Key == ConsoleKey.Escape)
+            {
+                var Admin = new Admin();
+                Admin.Show();
+
+            }
+        }
+        public void Serialize(ResOverzicht resoverzicht)
+        {
+            var serializeOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            var AdminOverzichtReserveringJson = JsonSerializer.Serialize(resoverzicht, serializeOptions);
+            File.WriteAllText("AdminOverzichtReservering.json", AdminOverzichtReserveringJson);
+        }
+    }
+}
