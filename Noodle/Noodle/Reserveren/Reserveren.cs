150f57f1a989ff8caea8bb0b121296dff48530ed
SophieKarreman <1038917@hr.nl>
2022-04-23 21:44:15 +0200

    Update TafelReserveren
    Engels toegevoegd + statements veranderd

--- Noodle/Noodle/Reserveren/Reserveren.cs
+++ Noodle/Noodle/Reserveren/Reserveren.cs
@@ -26,8 +26,8 @@ namespace Noodle
             {
                 //Leeg maken van de console
                 Console.Clear();
-                string taalSetting = "nl";
-          
+                string taalSetting = "en";
+
 
 
                 //Als het in het Nederlands staat
@@ -64,13 +64,13 @@ namespace Noodle
                     //Controleert of de maand klopt, loopt van 1 tot en met 12
                     while (validMonth == false)
                     {
-                        
+
                         reservatieDatumMaand = Convert.ToInt32(Console.ReadLine());
                         if (reservatieDatumMaand <= 12 && reservatieDatumMaand > 0)
                         {
                             validMonth = true;
                         }
-                        else if(reservatieDatumMaand == int){
+                        else if (reservatieDatumMaand == 9) {
                             Display("peop");
                         }
                         else
@@ -130,16 +130,23 @@ namespace Noodle
 
 
 
-                    Display("\nWelke tijd wilt u komen? (uur:minuut). U kunt per half uur reserveren.");
+                    Display("\nWelke tijd wilt u komen? (uur:minuut). U kunt per half uur reserveren. De eerst mogelijke tijd is 17:00. Reserveren kan tot 22:00.");
                     //Controleert of de tijd klopt.
-                    reservatieTijd =Console.ReadLine();
-                    if (reservatieTijd != "17:00" || reservatieTijd != "")
-                    {
-                       ;
-                    }
-                    else if (reservatieTijd == null)
-                    {
 
+                    bool validTime = false;
+                    while(validTime == false)
+                    {
+                        reservatieTijd = Console.ReadLine();
+                        if (reservatieTijd == "17:00" || reservatieTijd == "17:30" || reservatieTijd == "18:00" || reservatieTijd == "18:30" || reservatieTijd == "19:00"
+                            || reservatieTijd == "19:30" || reservatieTijd == "20:00" || reservatieTijd == "20:30" || reservatieTijd == "21:00" || reservatieTijd == "21:30" || reservatieTijd == "22:00")
+                        {
+                            validTime = true;
+                        }
+                        else
+                        {
+                            Display("\nDit is geen geldig tijd. Let op dat het binnen de reserveertijden valt. Er kan alleen per half uur gereserveerd worden. Noteer het op de volgende manier (uur:minuut)");
+                            validTime = false;
+                        }
                     }
 
 
@@ -150,8 +157,172 @@ namespace Noodle
                     {
                         Display("Voor reserveringen van meer dan 4 personen moet u even bellen naar het restaurant.");
                     }
+
+                    Display("\nBedankt voor je reservering! Klik op [ENTER] om de reservering te bevestigen!");
+                    input = Console.ReadKey();
+
+                    //Met escape terug aan het einde
+                    input = Console.ReadKey();
+                    if (input.Key == ConsoleKey.Enter)
+                    {
+                        string currentYear = DateTime.Now.Year.ToString();
+                        Console.Clear();
+                        Display("Bedankt voor het reserveren.");
+                        Display($"\nNaam: {reservatieNaam} \nDatum: {reservatieDatumDag}-{reservatieDatumMaand}-{currentYear} \nTijd: {reservatieTijd}\nAantal personen: {reservatieAantal}");
+
+
+
+
+                    }
                 }
-                
+
+                if (taalSetting == "en")
+                {
+                    Display("Thanks for choosing 'The Noodle' \nTo make a reservation press [ENTER]");
+                    Display("");
+
+                    //Enter om te beginnen met reserveren
+                    do
+                    {
+                        //Display dat je op enter moet klikken om terug te gaan
+                        Display("Press [ENTER] to begin with the reservation");
+                        input = Console.ReadKey();
+
+                        //Met escape terug aan het einde
+                        input = Console.ReadKey();
+                        if (input.Key == ConsoleKey.Escape)
+                        {
+                            var MainMenu = new MainMenu();
+                            MainMenu.Show();
+
+                        }
+                    }
+                    while (input.Key != ConsoleKey.Enter);
+
+                    Display("\nPlease, fill in your name:");
+
+                    //Invullen van de naam
+                    reservatieNaam = Console.ReadLine();
+                    Display("\nWhat month would you like to come?");
+                    bool validMonth = false;
+
+                    //Controleert of de maand klopt, loopt van 1 tot en met 12
+                    while (validMonth == false)
+                    {
+
+                        reservatieDatumMaand = Convert.ToInt32(Console.ReadLine());
+                        if (reservatieDatumMaand <= 12 && reservatieDatumMaand > 0)
+                        {
+                            validMonth = true;
+                        }
+                        else if (reservatieDatumMaand == 9)
+                        {
+                            Display("peop");
+                        }
+                        else
+                        {
+                            Display("That month doesn't seem to exist. Please fill in a month between 1 (januari) and 12 (december)");
+                            validMonth = false;
+                        }
+                    }
+                    Display("\nWhat day would you like to come?");
+
+                    //Controleert of de dag klopt. Met een max van het aantal dagen in die maand
+                    bool validDay = false;
+                    while (validDay == false)
+                    {
+                        reservatieDatumDag = Convert.ToInt32(Console.ReadLine());
+                        //  Maanden met 31 dagen
+                        if (reservatieDatumMaand == 1 || reservatieDatumMaand == 3 || reservatieDatumMaand == 5 || reservatieDatumMaand == 7 || reservatieDatumMaand == 8 || reservatieDatumMaand == 10 || reservatieDatumMaand == 12)
+                        {
+                            if (reservatieDatumDag < 0 || reservatieDatumDag > 31)
+                            {
+                                Display("\n This isn't a valid day for the month. Please fill in a number between 1 and 31.");
+                                validDay = false;
+
+                            }
+                            else
+                            {
+                                validDay = true;
+                            }
+                        }
+                        // Maanden met 30 dagen
+                        else if (reservatieDatumMaand == 4 || reservatieDatumMaand == 6 || reservatieDatumMaand == 9 || reservatieDatumMaand == 11)
+                        {
+                            if (reservatieDatumDag < 0 || reservatieDatumMaand > 30)
+                            {
+                                Display("\nThis isn't a valid day for the month. Please fill in a number between 1 and 30.");
+                                validDay = false;
+                            }
+                            else
+                            {
+                                validDay = true;
+                            }
+                        }
+                        //Februari
+                        else if (reservatieDatumMaand == 2)
+                        {
+                            if (reservatieDatumDag < 0 || reservatieDatumDag > 28)
+                            {
+                                Display("\nThis isn't a valid day for the month. Please fill in a number between 1 and 28.");
+                                validDay = false;
+                            }
+                            else
+                            {
+                                validDay = true;
+                            }
+                        }
+                    }
+
+
+
+                    Display("\nWhat time would you like to come (hour:minute). You can make a reservation per 30 minutes. The earliest opportunity is at 17:00. The last opportunity is at 22:00. ");
+                        
+                    //Controleert of de tijd klopt.
+
+                    bool validTime = false;
+                    while (validTime == false)
+                    {
+                        reservatieTijd = Console.ReadLine();
+                        if (reservatieTijd == "17:00" || reservatieTijd == "17:30" || reservatieTijd == "18:00" || reservatieTijd == "18:30" || reservatieTijd == "19:00"
+                            || reservatieTijd == "19:30" || reservatieTijd == "20:00" || reservatieTijd == "20:30" || reservatieTijd == "21:00" || reservatieTijd == "21:30" || reservatieTijd == "22:00")
+                        {
+                            validTime = true;
+                        }
+                        else
+                        {
+                            Display("\n This is not a valid time. Make sure to choose a time within the reservation hours. You can make a reservation per 30 minutes with the syntax hour:minutes");
+                            validTime = false;
+                        }
+                    }
+
+
+                    Display("\nWith how many people would you like to come?");
+                    //Controleert met hoeveel mensen ze komen. De tafels hebben maximaal 4 plekken. Alles daarboven ervoor bellen
+                    reservatieAantal = Convert.ToInt32(Console.ReadLine());
+                    if (reservatieAantal > 4)
+                    {
+                        Display("For reservations with more then 6 people.");
+                    }
+
+                    Display("\nThanks for filling in your information. Press [ENTER] To confirm it!");
+                    input = Console.ReadKey();
+
+                    //Met escape terug aan het einde
+                    input = Console.ReadKey();
+                    if (input.Key == ConsoleKey.Enter)
+                    {
+                        string currentYear = DateTime.Now.Year.ToString();
+                        Console.Clear();
+                        Display("Thanks for making a reservation at 'The noodle'");
+                        Display($"\nName: {reservatieNaam} \nDate: {reservatieDatumDag}-{reservatieDatumMaand}-{currentYear} \nTime: {reservatieTijd}\nHow many people: {reservatieAantal}");
+
+
+
+
+                    }
+                }
+
             }
             while (reservatieNaam == null || reservatieDatumMaand == 0 || reservatieTijd == null || reservatieAantal == 0);
 
@@ -164,8 +335,6 @@ namespace Noodle
                 MainMenu.Show();
 
             }
-
         }
-
     }
 }
